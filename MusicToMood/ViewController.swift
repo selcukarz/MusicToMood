5212f2f96440cc6c0ad1eb366f01821db68efdb0
selcukarz <ariozselcuk@gmail.com>
2024-03-21 17:58:56 +0300

    first commit

---
+++ MusicToMood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicToMood
+//
+//  Created by Selçuk Arıöz on 21.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
